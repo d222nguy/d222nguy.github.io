c97900b6bd087453ccdb2fa10f0e4538597763bc
Duy Nguyen <d222nguy@uwaterloo.ca>
2020-08-21 00:39:40 -0400

    add debug code to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import initSr from './js/sr';
 import './style/main.scss';
 
 $('a[href^="#"]').on('click', function(event) {
+  alert("hi");
   var target = $(this.getAttribute('href'));
   if (target.length) {
     event.preventDefault();
